8f16d2520adbddfeccb29f92b684a3c1461b5202
Sahil Gupta <guptas3067@gmail.com>
2025-10-26 19:03:02 +0530

    8

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import tailwindcss from "@tailwindcss/vite";
-import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import webpackStatsPlugin from "rollup-plugin-webpack-stats";
@@ -16,10 +15,10 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
-    nitroV2Plugin({
-      preset: target || "node-server",
-      compatibilityDate: "2025-10-26",
-    }),
+    // nitroV2Plugin({
+    //   preset: target || "node-server",
+    //   compatibilityDate: "2025-10-26",
+    // }),
     viteReact(),
     webpackStatsPlugin(),
   ],
