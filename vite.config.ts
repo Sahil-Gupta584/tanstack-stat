6cc1c047909c8dc56fbb0da267c8bd75b1aa6cf1
Sahil Gupta <guptas3067@gmail.com>
2025-10-27 22:23:49 +0530

    b

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
-const target = process.env.TARGET;
+const target = process.env.TARGET || "vercel";
 console.log({ target });
 
 const config = defineConfig({
@@ -18,7 +18,7 @@ const config = defineConfig({
     ...(target === "vercel"
       ? [
           nitroV2Plugin({
-            preset: "vercel",
+            preset: "node-server",
             compatibilityDate: "2025-10-26",
           }),
         ]
@@ -26,10 +26,10 @@ const config = defineConfig({
     viteReact(),
   ],
   server: { allowedHosts: ["08ee8c69800a.ngrok-free.app"] },
-  ssr: { external: ["mapbox-gl", "@faker-js"] },
+  ssr: { external: ["@faker-js"] },
   build: {
     rollupOptions: {
-      external: ["mapbox-gl", "@faker-js"],
+      external: ["@faker-js"],
     },
     sourcemap: false,
     chunkSizeWarningLimit: 1000,
