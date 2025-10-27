45eaecf695a1d8e76a464d093c11133210d31f40
Sahil Gupta <guptas3067@gmail.com>
2025-10-27 21:57:02 +0530

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import tailwindcss from "@tailwindcss/vite";
-import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
-import webpackStatsPlugin from "rollup-plugin-webpack-stats";
 import { defineConfig } from "vite";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
@@ -15,13 +13,13 @@ const config = defineConfig({
       projects: ["./tsconfig.json"],
     }),
     tailwindcss(),
-    tanstackStart(),
-    nitroV2Plugin({
-      preset: target || "node-server",
-      compatibilityDate: "2025-10-26",
-    }),
+    tanstackStart({}),
+    // nitroV2Plugin({
+    //   preset: target || "node-server",
+    //   compatibilityDate: "2025-10-26",
+    // }),
     viteReact(),
-    webpackStatsPlugin(),
+    // webpackStatsPlugin(),
   ],
   server: { allowedHosts: ["08ee8c69800a.ngrok-free.app"] },
   ssr: { external: ["mapbox-gl", "@faker-js"] },
