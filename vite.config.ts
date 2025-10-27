c4ff78124960ef28e7810cb1e42b4eddac93d37f
Sahil Gupta <guptas3067@gmail.com>
2025-10-27 22:09:27 +0530

    b

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -14,12 +15,15 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart({}),
-    // nitroV2Plugin({
-    //   preset: target || "node-server",
-    //   compatibilityDate: "2025-10-26",
-    // }),
+    ...(target === "vercel"
+      ? [
+          nitroV2Plugin({
+            preset: "vercel",
+            compatibilityDate: "2025-10-26",
+          }),
+        ]
+      : []),
     viteReact(),
-    // webpackStatsPlugin(),
   ],
   server: { allowedHosts: ["08ee8c69800a.ngrok-free.app"] },
   ssr: { external: ["mapbox-gl", "@faker-js"] },
