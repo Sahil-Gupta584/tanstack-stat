d7fd016845af965c924403ddbfcd75ecd1ef0736
Sahil Gupta <guptas3067@gmail.com>
2025-10-28 09:16:29 +0530

    f

--- src/configs/redis.ts
+++ src/configs/redis.ts
@@ -1,73 +1,85 @@
-import { getDateName } from '@/lib/utils/server'
-import { createClient } from 'redis'
+import { getDateName } from "@/lib/utils/server";
+import { createServerFn } from "@tanstack/react-start";
+import { createClient } from "redis";
 
-export async function getRedis() {
-  // return null
-  try {
-    const redis = await createClient({ url: process.env.REDIS_URL }).connect()
-    return redis
-  } catch (error) {
-    console.log('Failed to create redis Client', error)
-  }
-}
+// const getRedisconst = createServerFn().handler(async () => {});
+export const getRedis = createServerFn().handler(async () => {
+  // try {
+  console.log("url", process.env.REDIS_URL);
+  const redis = createClient({ url: process.env.REDIS_URL });
+  console.log("urls", process.env.REDIS_URL);
 
+  // redis.on("error", (err) => console.error("Redis Client Error:", err));
+
+  // await redis.connect();
+
+  // // Log host info
+  // const options = redis.options?.socket;
+  // console.log(`✅ Connected to Redis at ${options?.host}:${options?.port}`);
+
+  return redis;
+  // } catch (error) {
+  //   console.error("❌ Failed to create Redis client:", error);
+  //   return null;
+  // }
+});
 interface TUpdateCacheData {
-  websiteId: string
-  type: 'visitors' | 'revenues'
-  revenue?: number
+  websiteId: string;
+  type: "visitors" | "revenues";
+  revenue?: number;
   data: {
-    referrer: string
-    countryCode: string
-    region: string
-    city: string
-    browser: string
-    os: string
-    device: string
-    page: string
-  }
+    referrer: string;
+    countryCode: string;
+    region: string;
+    city: string;
+    browser: string;
+    os: string;
+    device: string;
+    page: string;
+  };
 }
 export async function updateCache(props: TUpdateCacheData) {
   try {
-    const { websiteId, data, revenue } = props
+    const { websiteId, data, revenue } = props;
     const { referrer, city, countryCode, region, browser, os, device, page } =
-      data
-    const redis = await createClient().connect()
+      data;
+    const redis = await createClient().connect();
     const cacheKeyPatterns = [
       `${websiteId}:main:*`,
       `${websiteId}:others:*`,
       `${websiteId}:goals*`,
-    ]
+    ];
 
     for (const patternKey of cacheKeyPatterns) {
-      const { keys } = await redis.scan('0', { MATCH: patternKey })
+      const { keys } = await redis.scan("0", { MATCH: patternKey });
 
       for (const key of keys) {
-        const cache = JSON.parse((await redis.get(key)) as string)
-        let name = ''
+        const cache = JSON.parse((await redis.get(key)) as string);
+        let name = "";
 
-        if (key.includes(':main:')) {
-          if (!Array.isArray(cache?.dataset)) return
+        if (key.includes(":main:")) {
+          if (!Array.isArray(cache?.dataset)) return;
           // console.log('exis', JSON.stringify(cache.dataset))
 
-          const date = new Date()
-          if (key.includes('days')) name = getDateName(date, 'last_7_days')
+          const date = new Date();
+          if (key.includes("days")) name = getDateName(date, "last_7_days");
 
           if (
-            key.includes('yesterday') ||
-            key.includes('today') ||
-            key.includes('last_24_hours')
+            key.includes("yesterday") ||
+            key.includes("today") ||
+            key.includes("last_24_hours")
           ) {
-            name = getDateName(date, 'last_24_hours')
+            name = getDateName(date, "last_24_hours");
           }
 
           const datasetRecord = cache.dataset?.findIndex(
-            (r: { name: string }) => r?.name === name,
-          ) as number
+            (r: { name: string }) => r?.name === name
+          ) as number;
 
           if (datasetRecord >= 0) {
-            cache.dataset[datasetRecord].visitors++
+            cache.dataset[datasetRecord].visitors++;
             if (revenue && revenue > 0) {
-              cache.dataset[datasetRecord].revenue += revenue
+              cache.dataset[datasetRecord].revenue += revenue;
             }
           } else {
             cache.dataset.push({
@@ -81,22 +93,22 @@ export async function updateCache(props: TUpdateCacheData) {
               sales: 0,
               goalCount: 0,
               timestamp: date.toISOString(),
-            })
+            });
           }
         }
 
-        if (key.includes(':others:')) {
-          if (!cache?.dataset) return
+        if (key.includes(":others:")) {
+          if (!cache?.dataset) return;
 
           const pageRecord = cache.dataset?.pageData?.findIndex(
-            (p: { label: string }) => p?.label === page,
-          )
+            (p: { label: string }) => p?.label === page
+          );
 
           if (pageRecord >= 0) {
-            cache.dataset.pageData[pageRecord].visitors++
+            cache.dataset.pageData[pageRecord].visitors++;
 
             if (revenue && revenue > 0)
-              cache.dataset.pageData[pageRecord].revenue += revenue
+              cache.dataset.pageData[pageRecord].revenue += revenue;
           } else {
             cache.dataset.pageData.push({
               label: page,
@@ -104,19 +116,19 @@ export async function updateCache(props: TUpdateCacheData) {
               revenue: 0,
               convertingVisitors: 1,
               conversionRate: 100,
-            })
+            });
           }
 
-          const hostname = referrer ? new URL(referrer).hostname : 'Direct'
+          const hostname = referrer ? new URL(referrer).hostname : "Direct";
           const referrerRecord = cache.dataset?.referrerData?.findIndex(
-            (r: { label: string }) => r?.label === hostname,
-          )
+            (r: { label: string }) => r?.label === hostname
+          );
 
           if (referrerRecord >= 0) {
-            cache.dataset.referrerData[referrerRecord].visitors++
+            cache.dataset.referrerData[referrerRecord].visitors++;
 
             if (revenue && revenue > 0)
-              cache.dataset.referrerData[referrerRecord].revenue += revenue
+              cache.dataset.referrerData[referrerRecord].revenue += revenue;
           } else {
             cache.dataset.referrerData.push({
               label: hostname,
@@ -125,19 +137,19 @@ export async function updateCache(props: TUpdateCacheData) {
               convertingVisitors: 1,
               conversionRate: 100,
               imageUrl: `https://icons.duckduckgo.com/ip3/${hostname}.ico`,
-            })
+            });
           }
 
-          const imageUrl = `https://purecatamphetamine.github.io/country-flag-icons/3x2/${countryCode}.svg`
+          const imageUrl = `https://purecatamphetamine.github.io/country-flag-icons/3x2/${countryCode}.svg`;
           const countryRecord = cache.dataset?.countryData?.findIndex(
             (p: { countryCode: string }) =>
-              p?.countryCode === countryCode || 'XX',
-          )
+              p?.countryCode === countryCode || "XX"
+          );
           if (countryRecord >= 0) {
-            cache.dataset.countryData[countryRecord].visitors++
+            cache.dataset.countryData[countryRecord].visitors++;
 
             if (revenue && revenue > 0)
-              cache.dataset.countryData[countryRecord].revenue += revenue
+              cache.dataset.countryData[countryRecord].revenue += revenue;
           } else {
             cache.dataset.countryData.push({
               label: countryCode,
@@ -146,17 +158,17 @@ export async function updateCache(props: TUpdateCacheData) {
               convertingVisitors: 1,
               conversionRate: 100,
               imageUrl,
-            })
+            });
           }
 
           const regionRecord = cache.dataset?.regionData?.findIndex(
-            (p: { label: string }) => p?.label === region,
-          )
+            (p: { label: string }) => p?.label === region
+          );
           if (regionRecord >= 0) {
-            cache.dataset.regionData[regionRecord].visitors++
+            cache.dataset.regionData[regionRecord].visitors++;
 
             if (revenue && revenue > 0)
-              cache.dataset.regionData[regionRecord].revenue += revenue
+              cache.dataset.regionData[regionRecord].revenue += revenue;
           } else {
             cache.dataset.regionData.push({
               label: region,
@@ -165,17 +177,17 @@ export async function updateCache(props: TUpdateCacheData) {
               convertingVisitors: 1,
               conversionRate: 100,
               imageUrl,
-            })
+            });
           }
 
           const cityRecord = cache.dataset?.cityData?.findIndex(
-            (p: { label: string }) => p?.label === city,
-          )
+            (p: { label: string }) => p?.label === city
+          );
           if (cityRecord >= 0) {
-            cache.dataset.cityData[cityRecord].visitors++
+            cache.dataset.cityData[cityRecord].visitors++;
 
             if (revenue && revenue > 0)
-              cache.dataset.cityData[cityRecord].revenue += revenue
+              cache.dataset.cityData[cityRecord].revenue += revenue;
           } else {
             cache.dataset.cityData.push({
               label: city,
@@ -184,17 +196,17 @@ export async function updateCache(props: TUpdateCacheData) {
               convertingVisitors: 1,
               conversionRate: 100,
               imageUrl,
-            })
+            });
           }
 
           const browserRecord = cache.dataset?.browserData?.findIndex(
-            (p: { label: string }) => p?.label === browser,
-          )
+            (p: { label: string }) => p?.label === browser
+          );
           if (browserRecord >= 0) {
-            cache.dataset.browserData[browserRecord].visitors++
+            cache.dataset.browserData[browserRecord].visitors++;
 
             if (revenue && revenue > 0)
-              cache.dataset.browserData[browserRecord].revenue += revenue
+              cache.dataset.browserData[browserRecord].revenue += revenue;
           } else {
             cache.dataset.browserData.push({
               label: browser,
@@ -203,17 +215,17 @@ export async function updateCache(props: TUpdateCacheData) {
               convertingVisitors: 1,
               conversionRate: 100,
               imageUrl,
-            })
+            });
           }
 
           const osRecord = cache.dataset?.osData?.findIndex(
-            (p: { label: string }) => p?.label === os,
-          )
+            (p: { label: string }) => p?.label === os
+          );
           if (osRecord >= 0) {
-            cache.dataset.osData[osRecord].visitors++
+            cache.dataset.osData[osRecord].visitors++;
 
             if (revenue && revenue > 0)
-              cache.dataset.osData[osRecord].revenue += revenue
+              cache.dataset.osData[osRecord].revenue += revenue;
           } else {
             cache.dataset.osData.push({
               label: os,
@@ -222,17 +234,17 @@ export async function updateCache(props: TUpdateCacheData) {
               convertingVisitors: 1,
               conversionRate: 100,
               imageUrl,
-            })
+            });
           }
 
           const deviceRecord = cache.dataset?.deviceData?.findIndex(
-            (p: { label: string }) => p?.label === device,
-          )
+            (p: { label: string }) => p?.label === device
+          );
           if (deviceRecord >= 0) {
-            cache.dataset.deviceData[deviceRecord].visitors++
+            cache.dataset.deviceData[deviceRecord].visitors++;
 
             if (revenue && revenue > 0)
-              cache.dataset.deviceData[deviceRecord].revenue += revenue
+              cache.dataset.deviceData[deviceRecord].revenue += revenue;
           } else {
             cache.dataset.deviceData.push({
               label: device,
@@ -241,16 +253,16 @@ export async function updateCache(props: TUpdateCacheData) {
               convertingVisitors: 1,
               conversionRate: 100,
               imageUrl,
-            })
+            });
           }
         }
 
-        await redis.set(key, JSON.stringify(cache))
+        await redis.set(key, JSON.stringify(cache));
       }
     }
   } catch (error) {
-    console.log('Failed to update cache', error, {
+    console.log("Failed to update cache", error, {
       props: JSON.stringify(props),
-    })
+    });
   }
 }
