b70527f51576a44c61bd5d1e7757d7f7dba49173
Sahil Gupta <guptas3067@gmail.com>
2026-05-04 00:55:45 +0530

    pricing page

---
+++ src/routes/pricing.tsx
@@ -0,0 +1,19 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { useUser } from "@/lib/userContext";
+import {
+  PricingSection,
+} from "./-components/landing";
+
+export const Route = createFileRoute("/")({
+    component: HomeComponent,
+});
+
+function HomeComponent() {
+    const { user } = useUser();
+
+    return (
+        <main className="bg-white dark:bg-[#0a0a0c] min-h-screen">
+            <PricingSection user={user} />
+        </main>
+    );
+}
